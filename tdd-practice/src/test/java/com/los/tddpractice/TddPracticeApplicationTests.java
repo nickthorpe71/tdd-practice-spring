751731f0e641e0051f9d6a296ab2ae4c421cb6b4
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-23 14:04:12 -0600

    init commit

---
+++ tdd-practice/src/test/java/com/los/tddpractice/TddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.los.tddpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
